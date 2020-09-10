915e39a5a9f018f839382a6d59a8869ba261165a
Collins Muriuki <murerwacollins@gmail.com>
2020-09-10 13:03:44 +0300

    Add barebones react app

---
+++ app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+    return  (
+        <h1>Hello from React</h1>
+)
+}
+
+export default App;
---
+++ app/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                // note that the order in which we insert loaders is important
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                },
+            },
+        ],
+    },
+    mode: "development",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+        new CopyWebpackPlugin({ patterns: ["index.html"] }),
+    ],
+};
