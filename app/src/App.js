fd838a54b2ddb738bbb8e38d6a07c7b5a616dec8
Collins Muriuki <murerwacollins@gmail.com>
2020-09-10 13:45:33 +0300

    Complete project

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+const wasm = import("wasm-pass");
 
 const App = () => {
-    return  (
-        <h1>Hello from React</h1>
-)
-}
+  const [password, genPassword] = useState("");
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleClick = () => {
+    wasm.then((wp) => {
+      genPassword(wp.generate(parseInt(input)));
+    }).catch(err => {
+        alert(err.toString())
+    });
+  };
+
+  return (
+    <div>
+      <p>Enter password length:</p>
+      <input type="number" onChange={handleChange} value={input} />
+      <button onClick={handleClick}>Generate Password</button>
+      <p>Your password:</p>
+      <strong>{password}</strong>
+    </div>
+  );
+};
 
 export default App;
